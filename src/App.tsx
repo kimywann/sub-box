a9220a6337f0995689cc49160533140f32cacdb4
kimywann <kimywan10@gmail.com>
2025-02-21 00:41:57 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="mt-1 ml-2.5 flex justify-between text-2xl text-blue-400">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
