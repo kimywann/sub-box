d4d7aa0946253ebf1bf534a5815f6436a5bc7fe9
kimywann <Kimywan10@gmail.com>
2025-02-21 21:23:13 +0900

    feat: Home 페이지에 소개 문구 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <>
+      <section className="bg-[linear-gradient(180deg,transparent_50%,rgba(0,100,255,.1))] p-96">
+        <p className="flex justify-center text-center text-4xl leading-tight font-bold whitespace-nowrap md:text-6xl">
+          구독에 관한 모든 것을 <br />
+          관리해 드릴게요!
+        </p>
+        <p className="flex justify-center pt-4 text-center text-2xl font-medium whitespace-nowrap text-gray-400">
+          다양한 구독을 해왔어서, <br /> 한 달에 총 비용이 얼마 나가는지.
+        </p>
+      </section>
+    </>
+  );
+};
+export default Home;
