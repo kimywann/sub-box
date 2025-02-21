5c7191971cf42111b3d220c626f281255dac4c30
kimywann <Kimywan10@gmail.com>
2025-02-21 21:23:43 +0900

    feat: 공통 레이아웃 컴포넌트 추가

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
