eff438ed5c0957dc79c3283319e559e4ebe4345e
kimywann <kimywan10@gmail.com>
2025-02-21 21:21:11 +0900

    feat: AuthButton 컴포넌트 UI 구현

---
+++ src/components/common/AuthButton.tsx
@@ -0,0 +1,14 @@
+interface AuthButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+const AuthButton = ({ children, onClick }: AuthButtonProps) => {
+  return (
+    <button className="cursor-pointer rounded-md px-3 py-2 text-sm text-gray-600 hover:bg-gray-100" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default AuthButton;
