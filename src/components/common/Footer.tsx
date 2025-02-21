e661dbb21887fc5b90f1613f09325434e76ee86c
kimywann <kimywan10@gmail.com>
2025-02-21 21:21:44 +0900

    feat: Footer 컴포넌트 UI 구현

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer className="m-0 h-48 bg-gray-50">
+      <section className="mx-auto flex h-full max-w-6xl items-center justify-between">
+        <div>
+          <p className="text-xs font-normal text-gray-500">ⓒ 2025. SubBox All rights reserved.</p>
+          <p className="mt-2 text-xs font-normal text-gray-500">이메일 team.xxxx@gmail.com</p>
+        </div>
+        <div>
+          <p className="text-xs font-normal text-gray-500">
+            <a href="https://github.com/kimywann/sub-box" target="_blank" rel="noopener noreferrer">
+              깃허브
+            </a>
+          </p>
+          <p className="mt-2 text-xs font-normal text-gray-500">서비스 피드백</p>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
