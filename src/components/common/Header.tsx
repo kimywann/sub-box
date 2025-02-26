75a883263aaa47a0a4b29361d50ad5e51d35b800
kimywann <kimywan10@gmail.com>
2025-02-26 19:54:44 +0900

    로그인 상태에 따른 헤더바 AuthButton 설정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,22 @@
-import { Link } from 'react-router-dom';
-import Button from './AuthButton';
+import AuthButton from './AuthButton';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { supabase } from '../../supabaseClient';
 
 const Header = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error('로그아웃 실패:', error.message);
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
     <header className="m-0 w-full p-3">
       <section className="mx-auto flex max-w-6xl justify-between">
@@ -11,12 +26,17 @@ const Header = () => {
           </Link>
         </div>
         <div>
-          <Link to="/signin">
-            <Button>로그인 | 회원가입</Button>
-          </Link>
+          {user ? (
+            <AuthButton onClick={handleLogout}>로그아웃</AuthButton>
+          ) : (
+            <Link to="/signin">
+              <AuthButton>로그인 | 회원가입</AuthButton>
+            </Link>
+          )}
         </div>
       </section>
     </header>
   );
 };
+
 export default Header;
