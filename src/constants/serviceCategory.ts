875763994ea0419200574acf870336e6feb6c94d
kimywann <kimywan10@gmail.com>
2025-02-26 20:43:20 +0900

    OTT 서비스 목록 상수 추가 및 이미지 경로 수정

---
+++ src/constants/serviceCategory.ts
@@ -0,0 +1,8 @@
+export const OttService = [
+  { name: 'Netflix', image: '/images/netflix.png' },
+  { name: 'Disney+', image: '/images/disney.png' },
+  { name: 'Wavve', image: '/images/wavve.png' },
+  { name: 'Tving', image: '/images/tving.png' },
+  { name: 'Watcha', image: '/images/watcha.png' },
+  { name: 'CoupangPlay', image: '/images/coupang.png' },
+];
