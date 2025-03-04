a443bfb610defb938dbc6400b1f8fb6d7ba83380
kimywann <Kimywan10@gmail.com>
2025-03-04 19:38:19 +0900

    SubscriptionData 인터페이스 분리 및 리팩토링

--- src/types/subscriptionData.ts
+++ src/types/subscriptionData.ts
@@ -1,8 +1,13 @@
-export interface SubscriptionData {
-  id: number;
-  user_email: string;
+// 모달에서 사용하는 구독 데이터 인터페이스
+export interface ModalSubscriptionData {
   service: string;
   subscription_date: string;
   price: number;
+}
+
+// 데이터베이스용 구독 데이터 인터페이스
+export interface DatabaseSubscriptionData extends ModalSubscriptionData {
+  id: number;
+  user_email: string;
   created_at: string;
 }
