255c44f12d9a488b9f9ff6bab377dfdfca2569c6
kimywann <kimywan10@gmail.com>
2025-03-04 21:36:34 +0900

    Home 문구 수정 및 Footer 피드백 링크

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,11 @@ const Home = () => {
     <>
       <section className="bg-[linear-gradient(180deg,transparent_50%,rgba(0,100,255,.1))] p-96">
         <p className="flex justify-center text-center text-4xl leading-tight font-bold whitespace-nowrap md:text-6xl">
-          구독에 관한 모든 것을 <br />
-          관리해 드릴게요!
+          모든 구독을 한눈에 👀 <br />
+          스마트하게 관리해요
         </p>
         <p className="flex justify-center pt-4 text-center text-2xl font-medium whitespace-nowrap text-gray-400">
-          다양한 구독을 해왔어서, <br /> 한 달에 총 비용이 얼마 나가는지
+          여러 구독 서비스를 한곳에서 관리하고 <br /> 매월 지출되는 비용을 정확히 파악하세요.
         </p>
       </section>
     </>
