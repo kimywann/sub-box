d364e91e1570e39d2f9f1115fea99940b32c4308
kimywann <Kimywan10@gmail.com>
2025-03-04 19:46:33 +0900

    Music 서비스 UI 구현

--- src/components/ServiceBox.tsx
+++ src/components/ServiceBox.tsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
-import { OttService } from '../constants/serviceCategory';
+import { OttService, MusicService, ServiceType } from '../constants/serviceCategory';
 import SubscriptionModal from './modal/SubscriptionModal';
-import { SubscriptionData } from '../types/subscriptionData';
+import { ModalSubscriptionData } from '../types/subscriptionData';
+
 interface ServiceBoxProps {
-  onSaveSubscription?: (data: SubscriptionData) => void;
+  onSaveSubscription?: (data: ModalSubscriptionData) => void;
 }
 
 const ServiceBox = ({ onSaveSubscription }: ServiceBoxProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedService, setSelectedService] = useState<(typeof OttService)[0] | null>(null);
+  const [selectedService, setSelectedService] = useState<ServiceType | null>(null);
 
-  const handleServiceClick = (service: (typeof OttService)[0]) => {
+  const handleServiceClick = (service: ServiceType) => {
     setSelectedService(service);
     setIsModalOpen(true);
   };
 
-  const handleSaveSubscription = (data: SubscriptionData) => {
+  const handleSaveSubscription = (data: ModalSubscriptionData) => {
     if (onSaveSubscription) {
       onSaveSubscription(data);
     }
@@ -28,6 +29,19 @@ const ServiceBox = ({ onSaveSubscription }: ServiceBoxProps) => {
           <h2 className="mb-4 text-xl font-bold text-gray-800">OTT</h2>
           <div className="flex flex-row gap-4 overflow-x-auto pb-4">
             {OttService.map((service) => (
+              <div
+                key={service.name}
+                className="flex h-32 w-32 flex-shrink-0 flex-col items-center justify-center rounded-md border border-gray-300 p-6 transition-colors hover:border-blue-500 hover:shadow-md"
+                onClick={() => handleServiceClick(service)}
+              >
+                <img src={service.image} alt={service.name} className="h-16 w-16 cursor-pointer object-contain" />
+                <span className="mt-2 text-sm">{service.name}</span>
+              </div>
+            ))}
+          </div>
+          <h2 className="mb-4 text-xl font-bold text-gray-800">Music</h2>
+          <div className="flex flex-row gap-4 overflow-x-auto pb-4">
+            {MusicService.map((service) => (
               <div
                 key={service.name}
                 className="flex h-32 w-32 flex-shrink-0 flex-col items-center justify-center rounded-md border border-gray-300 p-6 transition-colors hover:border-blue-500 hover:shadow-md"
