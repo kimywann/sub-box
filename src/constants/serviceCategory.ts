e6ba8fce3674c147ebffb304a645bc6a632ca80d
kimywann <kimywan10@gmail.com>
2025-03-04 19:44:38 +0900

    ServiceType 타입 추가 및 Music 서비스 추가

--- src/constants/serviceCategory.ts
+++ src/constants/serviceCategory.ts
@@ -1,8 +1,27 @@
+export type ServiceType = {
+  name: string;
+  image: string;
+};
+
 export const OttService = [
-  { name: 'Netflix', image: '/images/netflix.png' },
-  { name: 'Disney+', image: '/images/disney.png' },
-  { name: 'Wavve', image: '/images/wavve.png' },
-  { name: 'Tving', image: '/images/tving.png' },
-  { name: 'Watcha', image: '/images/watcha.png' },
-  { name: 'CoupangPlay', image: '/images/coupang.png' },
+  { name: 'Netflix', image: '/images/ott/Netflix.png' },
+  { name: 'Disney+', image: '/images/ott/Disney.png' },
+  { name: 'Wavve', image: '/images/ott/Wavve.png' },
+  { name: 'Tving', image: '/images/ott/Tving.png' },
+  { name: 'Watcha', image: '/images/ott/Watcha.png' },
+  { name: 'CoupangPlay', image: '/images/ott/CoupangPlay.png' },
+  { name: 'AppleTV+', image: '/images/ott/AppleTV+.jpeg' },
+  { name: 'PrimeVideo', image: '/images/ott/PrimeVideo.png' },
+];
+
+export const MusicService = [
+  { name: 'YoutubeMusic', image: '/images/music/YoutubeMusic.png' },
+  { name: 'AppleMusic', image: '/images/music/AppleMusic.jpeg' },
+  { name: 'Spotify', image: '/images/music/Spotify.png' },
+  { name: 'Melon', image: '/images/music/Melon.png' },
+  { name: 'Genie', image: '/images/music/Genie.png' },
+  { name: 'Bugs', image: '/images/music/Bugs.jpeg' },
+  { name: 'VIBE', image: '/images/music/Vibe.jpeg' },
+  { name: 'FLO', image: '/images/music/Flo.png' },
+  { name: 'AmazonMusic', image: '/images/music/AmazonMusic.jpeg' },
 ];
