6a1dd5e77f7ca0c895bdc116d38edd9be700824f
kimywann <kimywan10@gmail.com>
2025-03-08 17:38:26 +0900

    BillingCycle 타입 추가

--- src/types/subscriptionData.ts
+++ src/types/subscriptionData.ts
@@ -1,13 +1,14 @@
-// 모달에서 사용하는 구독 데이터 인터페이스
 export interface ModalSubscriptionData {
   service: string;
   subscription_date: string;
   price: number;
+  billing_cycle: BillingCycle;
 }
 
-// 데이터베이스용 구독 데이터 인터페이스
 export interface DatabaseSubscriptionData extends ModalSubscriptionData {
   id: number;
   user_email: string;
   created_at: string;
 }
+
+export type BillingCycle = 'weekly' | 'biweekly' | 'monthly' | 'yearly';
